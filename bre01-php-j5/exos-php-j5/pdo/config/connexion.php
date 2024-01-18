b8072ce53a503a439a357ffeb12cad912ac042c3
m3c-git <ayyoubentreprise@gmail.com>
2024-01-18 13:32:24 +0100

    update

--- bre01-php-j5/exos-php-j5/pdo/config/connexion.php
+++ bre01-php-j5/exos-php-j5/pdo/config/connexion.php
@@ -2,7 +2,7 @@
 $host = "localhost"; //host pour phpmyadmin de la 3wa =>  "db.3wa.io"
 $port = "3306";
 $dbname = "eddyfrair_phpj5";
-$connexionString = "mysql:host=$host$;port=$port;dbname=$dbname";
+$connexionString = "mysql:host=$host;port=$port;dbname=$dbname";
 
 $user = "root";
 $password = "";
